e2734d3d8884af5005882f72110f3a9f4384b35b
Gwaple <349313809@gapps.yrdsb.ca>
2025-06-27 13:27:30 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,4 @@
-let currentUser = null;
 
-// --- Account Management ---
-function hash(str) {
-  let h = 0; for (let i = 0; i < str.length; i++) h = Math.imul(31, h) + str.charCodeAt(i) | 0; return h.toString();
-}
 function getAccounts() {
   return JSON.parse(localStorage.getItem("garden_accounts") || "{}");
 }
